73d95532ab3b8a5fd3d3a3fdf3f352afdda27250
mofath <mohamed.elgaedi@gmail.com>
2021-01-29 18:59:02 +0200

    Setting up sever

---
+++ server.js
@@ -0,0 +1,54 @@
+var app = require("./app");
+var http = require("http");
+
+const dotEnv = require("dotenv");
+
+dotEnv.config();
+ 
+/**
+ * Get port from environment and store in Express.
+ */
+
+const PORT = process.env.PORT || 3000;
+app.set("port", PORT);
+
+const server = http.createServer(app);
+server.listen(PORT);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
+}
