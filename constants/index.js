303180afe83fe4250c2d7e95e917815c358e97d7
mofath <mohamed.elgaedi@gmail.com>
2021-02-01 02:35:27 +0200

    Added Error and message constants

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  HttpStatus: require("./error_constants").HttpStatus,
+  STATUS_MAP_MESSAGE: require("./error_constants").STATUS_MAP_MESSAGE,
+  databaseMessage: require("./messages_constants").databaseMessage,
+  databaseMessage: require("./messages_constants").productMessage,
+  databaseMessage: require("./messages_constants").requestValidationMessage,
+  databaseMessage: require("./messages_constants").userMessage,
+};
---
+++ constants/messages_constants.js
@@ -0,0 +1,21 @@
+module.exports.productMessage = {
+  PRODUCT_CREATED: "Product Created Successfully",
+  PRODUCT_FETCHED: "Product Fetched Successfully",
+  PRODUCT_UPDATED: "Product Updated Successfully",
+  PRODUCT_DELETED: "Product Deleted Successfully",
+  PRODUCT_NOT_FOUND: "Product Not Found",
+};
+module.exports.userMessage = {
+  SIGNUP_SUCCESS: "Signup Success",
+  LOGIN_SUCCESS: "Login Success",
+  DUPLICATE_EMAIL: "User already exist with given email",
+  USER_NOT_FOUND: "User not found",
+  INVALID_PASSWORD: "Incorrect Password",
+};
+module.exports.requestValidationMessage = {
+  BAD_REQUEST: "Invalid fields",
+  TOKEN_MISSING: "Token missing from header",
+};
+module.exports.databaseMessage = {
+  INVALID_ID: "Invalid Id",
+};
