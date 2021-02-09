a59f047e6010ab552137bc9f523c93e9f7eef01e
mofath <mohamed.elgaedi@gmail.com>
2021-02-09 19:35:28 +0200

    Adjusted Error handing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/product", require("./routes/productRoutes"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -22,7 +23,7 @@ app.use(function (req, res, next) {
 // will print stacktrace
 if (app.get("env") === "development") {
   app.use(function (err, req, res, next) {
-    console.log(err);
+    console.log(err.message);
     res.status(err.status || 500);
     return res.json({
       message: err.message,
