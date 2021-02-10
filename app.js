aa41a81fc5610aaecaf37d5fed5745ce16da3771
mofath <mohamed.elgaedi@gmail.com>
2021-02-11 01:51:42 +0200

    Log requests and error

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require("express");
-const { NotFoundError } = require("./lib/ErrorHandler");
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./swagger.yaml");
+const { NotFoundError } = require("./lib/ErrorHandler");
+const { accessLog, errorLog } = require("./lib/logger");
 
 const app = express();
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // request payload middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add a request logging middleware in development mode
+if (app.get("env") === "development") {
+  app.use((req, res, next) => {
+    accessLog.info(`${req.method}: ${req.url}`);
+    return next();
+  });
+}
+
 app.use("/api/v1/product", require("./routes/productRoutes"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 
 // API documentation
-app.use("/api-docs/",  swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -29,7 +39,7 @@ app.use(function (req, res, next) {
 // will print stacktrace
 if (app.get("env") === "development") {
   app.use(function (err, req, res, next) {
-    console.log(err.message);
+    errorLog.error(err.message);
     res.status(err.status || 500);
     return res.json({
       message: err.message,
